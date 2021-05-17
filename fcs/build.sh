343b632c615c35745aba4bc5f7ab261f27f2b432
ncave <777696+ncave@users.noreply.github.com>
2021-05-16 21:10:07 -0700

    Cleanup comments

--- fcs/build.sh
+++ fcs/build.sh
@@ -13,12 +13,20 @@ dotnet build -c Release
 dotnet run -c Release -- ../../../src/fsharp/FSComp.txt FSComp.fs
 dotnet run -c Release -- ../../../src/fsharp/fsi/FSIstrings.txt FSIstrings.fs
 
+# cleanup comments
+files="FSComp.fs FSIstrings.fs"
+for file in $files; do
+  echo "Delete comments in $file"
+  sed -i '1s/^\xEF\xBB\xBF//' $file # remove BOM
+  sed -i '/^ *\/\//d' $file # delete all comment lines
+done
+
 # replace all #line directives with comments
 files="lex.fs pplex.fs illex.fs ilpars.fs pars.fs pppars.fs"
 for file in $files; do
   echo "Replace #line directives with comments in $file"
-  sed -i 's/^# [0-9]/\/\/\0/' $file                           # comment #line directives
-  sed -i 's/^\(\/\/# [0-9]\{1,\} "\).*\/codegen\//\1/' $file  # cleanup #line paths
+  sed -i 's/^# [0-9]/\/\/\0/' $file # comment all #line directives
+  sed -i 's/^\(\/\/# [0-9]\{1,\} "\).*\/codegen\/\(\.\.\/\)*/\1/' $file # cleanup #line paths
 done
 
 # FCS-Fable build
